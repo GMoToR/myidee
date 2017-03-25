3ff5b44a15c4d413e634661fcca4e2e9d1690e97
GMoToR <gavrilenko.jeka@mail.ru>
2017-03-25 12:35:11 +0200

    carusel

--- application/models/Model_myidee.php
+++ application/models/Model_myidee.php
@@ -13,5 +13,13 @@ class model_myidee extends CI_Model {
         // возвращаем id последнего вставленного элемента
         return true;
     }
-
+  	public function getPost($content) {
+        // вставляем в таблицу массив с данными, в качестве имени для каждого элемента массива указываем имя столбца куда нужно записать значение
+        $email = $content['email'];
+        $name = $content['name'];
+        $review = $content['review'];
+        $this->db->insert('myidee_table', array('email' => $email, 'name'  => $name, 'review' => $review));
+        // возвращаем id последнего вставленного элемента
+    return true;
+    }
 }
--- application/views/idee.php
+++ application/views/idee.php
@@ -6,11 +6,11 @@
   <link rel="stylesheet" type="text/css" href="/css/style.css">
   <!-- Optional theme -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-
   <!-- Latest compiled and minified JavaScript -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
   <script type="text/javascript" src="/js/jquery.min.js"></script>
-	<script type="text/javascript" src="/js/script.js"></script>
+  <script type="text/javascript" src="/js/script.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 
   <title>Comming soon</title>
@@ -150,17 +150,63 @@
 				<div class="underlinebig"></div>
 			</div>
 		</div>
-		
+					<!-- Карусель -->
+			<div id="myCarousel" class="carousel slide" data-interval="5000" data-ride="carousel">
+			  <!-- Индикаторы для карусели -->
+			  <ol class="carousel-indicators">
+			    <!-- Перейти к 0 слайду карусели с помощью соответствующего индекса data-slide-to="0" -->
+			    <li class=" active" data-target="#myCarousel" data-slide-to="0" ></li>
+			    <!-- Перейти к 1 слайду карусели с помощью соответствующего индекса data-slide-to="1" -->
+			    <li class="" data-target="#myCarousel" data-slide-to="1" ></li>
+			    <!-- Перейти к 2 слайду карусели с помощью соответствующего индекса data-slide-to="2" -->
+			    <li class="" data-target="#myCarousel" data-slide-to="2"></li>
+
+			  </ol>   
+			  <!-- Слайды карусели -->
+			  <div class="carousel-inner" role="listbox">
+			    <!-- Слайды создаются с помощью контейнера с классом item, внутри которого помещается содержимое слайда -->
+			    <div class="active item">
+			      <div class="carousel-caption">
+			        <h3>Заголовок 1 слайда</h3>
+			        <p>Текст (описание) 1 слайда...</p>
+			      </div>
+			    </div>
+			    <!-- Слайд №2 -->
+			    <div class=" item">
+			      <div class="carousel-caption">
+			        <h3>Second slide label</h3>
+			        <p>Aliquam sit amet gravida nibh, facilisis gravida odio.</p>
+			      </div>
+			    </div>
+			    <!-- Слайд №3 -->
+			    <div class="item">
+			      <div class="carousel-caption">
+			        <h3>Third slide label</h3>
+			        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+			      </div>
+			    </div>
+			  </div>
+			  <!-- Навигация для карусели -->
+			  <!-- Кнопка, осуществляющая переход на предыдущий слайд с помощью атрибута data-slide="prev" -->
+			  <a class="carousel-control left" href="#myCarousel" role="button" data-slide="prev">
+			    <span class="glyphicon glyphicon-chevron-left"></span>
+			  </a>
+			  <!-- Кнопка, осуществляющая переход на следующий слайд с помощью атрибута data-slide="next" -->
+			  <a class="carousel-control right" href="#myCarousel" role="button" data-slide="next">
+			    <span class="glyphicon glyphicon-chevron-right"></span>
+			  </a>
+			</div>
+
 	</div>
 	<div id="bg6">
 		<div class="row">
 		<form>
 			<div class="col-md-5 col-sm-5">
-					<input name="email_in" id="email_in" type="email" placeholder="Введите ваш e-mail">
+					<input name="email_in" id="email_in" type="email" placeholder="Введите ваш e-mail" required>
 					<input name="name_in" id="name_in" type="textarea" placeholder="Введите ваше имя">
 			</div>
 			<div class="col-md-7 col-sm-7">
-					<textarea name="review_in" id="review_in" placeholder="Введите ваше сообщение"></textarea>
+					<textarea name="review_in" id="review_in" type="textarea" placeholder="Введите ваше сообщение"></textarea>
 					<input name="content_submit" id="content_submit" type="submit">
 			</div>
 			</form>
